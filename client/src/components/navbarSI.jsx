c398793f7c6384b560673166f3607f42faed30c4
bencsci <benl275133@gmail.com>
2025-03-02 01:19:30 -0400

    Fix mobile nav to close when switching pages

--- client/src/components/navbarSI.jsx
+++ client/src/components/navbarSI.jsx
@@ -63,18 +63,21 @@ const NavbarSI = () => {
             <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
               <Link
                 to="/"
+                onClick={() => setIsOpen(false)}
                 className="block text-gray-600 hover:text-red-500 px-3 py-2 rounded-md font-medium"
               >
                 Chat
               </Link>
               <Link
                 to="/review"
+                onClick={() => setIsOpen(false)}
                 className="block text-gray-600 hover:text-red-500 px-3 py-2 rounded-md font-medium"
               >
                 Review
               </Link>
               <Link
                 to="/settings"
+                onClick={() => setIsOpen(false)}
                 className="block text-gray-600 hover:text-red-500 px-3 py-2 rounded-md font-medium"
               >
                 Settings
