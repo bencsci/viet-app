a2f80416c8258c6ce341e7854376f096f1a8e413
bencsci <benl275133@gmail.com>
2025-03-24 21:50:25 -0300

    Fixed TTS for Vercel

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -8,9 +8,52 @@ import { TranslationServiceClient } from "@google-cloud/translate";
 import textToSpeech from "@google-cloud/text-to-speech";
 import { getUserProfile, getLanguageSettings } from "../functions/user.js";
 import { supabaseClient } from "../config/supabaseClient.js";
+import { ExternalAccountClient } from "google-auth-library";
+import { getVercelOidcToken } from "@vercel/functions/oidc";
 
-const translationClient = new TranslationServiceClient();
-const speechClient = new textToSpeech.TextToSpeechClient();
+const GCP_PROJECT_ID = process.env.GCP_PROJECT_ID;
+const GCP_PROJECT_NUMBER = process.env.GCP_PROJECT_NUMBER;
+const GCP_SERVICE_ACCOUNT_EMAIL = process.env.GCP_SERVICE_ACCOUNT_EMAIL;
+const GCP_WORKLOAD_IDENTITY_POOL_ID = process.env.GCP_WORKLOAD_IDENTITY_POOL_ID;
+const GCP_WORKLOAD_IDENTITY_POOL_PROVIDER_ID =
+  process.env.GCP_WORKLOAD_IDENTITY_POOL_PROVIDER_ID;
+
+// Initialize the External Account Client
+const authClient = ExternalAccountClient.fromJSON({
+  type: "external_account",
+  audience: `//iam.googleapis.com/projects/${GCP_PROJECT_NUMBER}/locations/global/workloadIdentityPools/${GCP_WORKLOAD_IDENTITY_POOL_ID}/providers/${GCP_WORKLOAD_IDENTITY_POOL_PROVIDER_ID}`,
+  subject_token_type: "urn:ietf:params:oauth:token-type:jwt",
+  token_url: "https://sts.googleapis.com/v1/token",
+  service_account_impersonation_url: `https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/${GCP_SERVICE_ACCOUNT_EMAIL}:generateAccessToken`,
+  subject_token_supplier: {
+    // Use the Vercel OIDC token as the subject token
+    getSubjectToken: getVercelOidcToken,
+  },
+});
+
+let translationClient;
+let speechClient;
+if (process.env.ENV === "production") {
+  translationClient = new TranslationServiceClient({
+    project: GCP_PROJECT_ID,
+    location: "us-central1",
+    googleAuthOptions: {
+      authClient,
+      projectId: GCP_PROJECT_ID,
+    },
+  });
+  speechClient = new textToSpeech.TextToSpeechClient({
+    project: GCP_PROJECT_ID,
+    location: "us-central1",
+    googleAuthOptions: {
+      authClient,
+      projectId: GCP_PROJECT_ID,
+    },
+  });
+} else {
+  translationClient = new TranslationServiceClient();
+  speechClient = new textToSpeech.TextToSpeechClient();
+}
 
 // Add error checking for credentials
 if (
